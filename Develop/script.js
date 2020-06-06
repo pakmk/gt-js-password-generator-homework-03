13956a1c5f6dbcc1b096c3b8aa98b5c90e8abd7b
min <pakmk8@gmail.com>
2020-06-06 18:22:44 -0400

    added passwordLength prompt.

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  passwordLength = prompt("How many characters long? (Must be between 8 and 128 characters.)");
+  if(passwordLength < 8 || passwordLength > 128){
+    alert("Length is invalid.");
+    return;
+  }
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
